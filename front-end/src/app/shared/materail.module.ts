ffa41900362428652ff07501ae237a42c8a79aac
Szabolcs Pongrácz <szabolcspongracz@Szabolcss-MacBook-Pro.local>
2021-05-23 22:31:35 +0200

    avatars + register

--- front-end/src/app/shared/materail.module.ts
+++ front-end/src/app/shared/materail.module.ts
@@ -5,6 +5,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [],
@@ -13,6 +14,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     MatIconModule,
     MatCardModule,
     MatProgressSpinnerModule,
+    MatInputModule,
   ],
 })
 export class MaterailModule {}
--- front-end/src/core-component/register/register.component.ts
+++ front-end/src/core-component/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { LoaderService } from '../global-loader/shared/loader.service';
 import { RegisterService } from './shared/register.service';
 
@@ -9,9 +9,22 @@ import { RegisterService } from './shared/register.service';
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  success = false;
+  error = false;
+
+  get userName() {
+    return this.profileForm.get('userName');
+  }
+  get password() {
+    return this.profileForm.get('password');
+  }
+
   profileForm = new FormGroup({
-    userName: new FormControl(''),
-    password: new FormControl(''),
+    userName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+    password: new FormControl('', Validators.required),
   });
 
   constructor(
@@ -22,17 +35,28 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {}
 
   submit() {
-    this.loaderService.isShow.next(true);
-    console.log(this.profileForm);
-    this.registerService
-      .register({
-        name: this.profileForm.get('userName').value,
-        password: this.profileForm.get('password').value,
-      })
-      .subscribe(
-        (data) => console.log('post answer: ', data),
-        (error) => console.log('ERROR: ', error),
-        () => this.loaderService.isShow.next(false)
-      );
+    if (this.profileForm.valid) {
+      this.loaderService.isShow.next(true);
+      console.log(this.profileForm);
+      this.registerService
+        .register({
+          name: this.profileForm.get('userName').value,
+          password: this.profileForm.get('password').value,
+        })
+        .subscribe(
+          (data) => console.log('post answer: ', data),
+          (error) => {
+            console.log('ERROR: ', error);
+            this.loaderService.isShow.next(false);
+            this.error = true;
+          },
+          () => {
+            this.loaderService.isShow.next(false);
+            this.success = true;
+          }
+        );
+    } else {
+      this.profileForm.markAllAsTouched();
+    }
   }
 }
